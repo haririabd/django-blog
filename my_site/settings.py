afd14025a4e80bdf873dc2a9a48fbd71a9da3014
haririabd <hariri.bin.abdullah@gmail.com>
2022-11-05 22:00:22 +0800

    working without database

--- my_site/settings.py
+++ my_site/settings.py
@@ -123,6 +123,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# We add static files for project level
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
